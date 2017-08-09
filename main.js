b395eeba34793c064d8f37b96b5066b1ad3fd778
ginniehench <ginniehench@gmail.com>
2017-08-08 22:56:38 -0400

    more things done. messing around trying to understand php files now

--- main.js
+++ main.js
@@ -1,11 +1,6 @@
 let searchInputEl = document.getElementById("search_field");
 let buttonEl = document.getElementById("search_now");
-
-// console.log(url_root + search_term);
-
-// let url_root = "http://www.recipepuppy.com/api/?q=";
-// let url = url_root + search_term;
-
+// let url = "http://recipepuppyproxy.herokuapp.com/api/?i=apples"; << don't need this here. Refer to the constant portion in the call.
 
 buttonEl.addEventListener('click', function(e) {
     let search_termVal = searchInputEl.value;
@@ -16,29 +11,34 @@ buttonEl.addEventListener('click', function(e) {
       searchInputEl.value = "";
     }
     console.log(search_termVal);
-    let url = "http://www.recipepuppy.com/api/?q="+search_termVal;
+    url = ("http://recipepuppyproxy.herokuapp.com/api/?q="+search_termVal);
     console.log(url);
-    return url;
+    // return search_termVal;
+
+    function fetchGET(url) {
+      fetch(url)
+        .then(function(response) {
+            if (response.status !== 200) {
+              console.log(response.status);
+              return;
+            }
+            response.json().then(function(data) {
+              console.log("define query return variables", response.url);
+              console.log(data.results);
+            });
+          })
+          .catch(function(err) {
+            console.log("Fetch Error :-S", err);
+          });
+        }
+
+    fetchGET(url); /*finally!!!! this works and this aligns with what I first learned about functions.*/
   });
 
-function fetchGET(url){
-  fetch(url)
-    .then(function(response) {
-        if (response.status !== 200) {
-          console.log(response.status);
-          return;
-        }
-        response.json().then(function(data) {
-          console.log("define query return variables", response.url);
-          console.log(data);
-        });
-      })
-      .catch(function(err) {
-        console.log("Fetch Error :-S", err);
-      });
-    }
+// console.log(search_termVal);
 
-fetchGET(url)
+
+// this line above is working to call fetchGET, when url is in the parameter and right after fetch(). Rearrange the eventlistener function to feed a variable to this function call.
 
 // http://recipepuppyproxy.herokuapp.com/api/?i=onions,garlic&q=omelet&p=3
 // fetchGET(url_root + search_term);
